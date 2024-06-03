cd927e42abfd2a2d6d03f5b8f7de87f7649560ec
Nanfeng <little_nanfeng@163.com>
2024-06-03 09:14:35 +0800

    6.3

---
+++ src/main/java/contorller/UserController.java
@@ -0,0 +1,37 @@
+package contorller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/loginCheck")
+    public String loginChenk(User user, HttpServletRequest request){
+
+        User user1=userService.queryUser(user);
+
+        if(user1!=null){
+            request.getSession().setAttribute("USER_SESSION",user1);
+            return "redirect:/main";
+        }
+        else {
+            request.setAttribute("msg","账号、密码或身份信息错误...");
+            return "login";
+        }
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request){
+        HttpSession session=request.getSession();
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
