69440587b2a22f285a9ecdb9096f98e3eb08e6be
xbx <1877106488@qq.com>
2024-06-05 14:34:56 +0800

    xx

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,4 +1,12 @@
 package service;
 
+import pojo.Book;
+import pojo.User;
+
+import java.util.List;
+
 public interface BookService {
+    public List<Book> queryBook(Book book);
+    public Book queryBookByIsbn(String isbn);
+    public Boolean borrowBook(Book book, User user);
 }
